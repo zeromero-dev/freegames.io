05a7f3cf58b69e04e5e8cf7875c0dce1185620e2
Roman <spiderr.roma@gmail.com>
2022-09-08 19:13:22 +0300

    added auth lib

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 import { Game } from '../src/components/Games'
 import { ScrollButton } from '../src/components/ScrollButton'
 import { Header } from '../src/components/Header'
+import { ThemeChanger } from '../src/components/ThemeChanger'
 
 const Home: NextPage = () => {
   return (
     <div>
       <Header />
+      {/* <ThemeChanger /> */}
       <Game />
       <ScrollButton />
     </div>
