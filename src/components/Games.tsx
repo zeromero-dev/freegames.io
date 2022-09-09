c7f01b01e52164b3c93a78212159407c0f3c1266
Roman <spiderr.roma@gmail.com>
2022-09-10 01:01:46 +0300

    save com for branching

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -7,29 +7,29 @@ import { useQuery, dehydrate, QueryClient } from '@tanstack/react-query'
 import { GameCard } from './GameCard';
 import { options } from './fetchers/options'
 
-export async function getServerSideProps() {
-  
-  const queryClient = new QueryClient()
+import { GameCardProps } from './GameCard';
 
-  await queryClient.prefetchQuery(['posts'], () => axios
-    .get("https://gamerpower.p.rapidapi.com/api/giveaways", options)
-    .then((res) => res.data))
-
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    }
-  }
+const fetchGames = async () => {
+  const res = await axios.get("https://gamerpower.p.rapidapi.com/api/giveaways", options)
+  return res.data
 }
+// The data that comes to server as "undefined" for some reason.
+// Needs fixing
+// export async function getServerSideProps() {
+//   const queryClient = new QueryClient()
+
+//   await queryClient.prefetchQuery(['gameData'], fetchGames)
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     }
+//   }
+// }
 
 export const Game = () => {
 
-  const { isLoading, error, data } = useQuery(["gameData"], () =>
-    axios
-      .get("https://gamerpower.p.rapidapi.com/api/giveaways", options)
-      .then((res) => res.data)
-  );
-  //I know it looks silly, I'll change it later
+  const { data, error, isLoading } = useQuery(["gameData"], fetchGames)
+
   if (isLoading) return (
     <div className="flex justify-center items-center">
       <div className="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin">
@@ -37,15 +37,18 @@ export const Game = () => {
     </div>
   )
 
-  if (error) return <div> ERROR</div>
+  error ? <div>Error</div> : null
+
   return (
     <div className='grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-2 gap-y-8 ml-10 grid-flow-dense mt-5'>
       {data.map((game: any) => {
         return (
-          <GameCard key={game.id} id={game.id} name={game.title} image={game.image} description={game.description} url={game.open_giveaway_url} platforms={game.platforms} /> //description.substring(0, 300)
+          <GameCard key={game.id} id={game.id} name={game.title} image={game.image} description={game.description} url={game.open_giveaway_url} platforms={game.platforms} />
         )
       })}
     </div>
   )
 }
+
+
 
