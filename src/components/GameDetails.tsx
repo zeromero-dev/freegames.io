fc8c2fcb20a1087eb5ced45c3186a5c3513324d8
Roman <spiderr.roma@gmail.com>
2022-09-27 18:14:54 +0300

    dynamic and simple layout implemented

--- src/components/GameDetails.tsx
+++ src/components/GameDetails.tsx
@@ -18,26 +18,46 @@ type Game = {
 
 
 const GameDetails = ({ name, image, description, id, url, platforms, instructions, status, type }: Game) => {
-  return (
-    <div>
-        <Header />
-        <div className="flex ">
-            <div className='flex-col m-6 ml-12'>
-                <ReactPlayer url="https://www.youtube.com/watch?v=c0i88t0Kacs" /> 
-                <div className=''>
-                    <Image src={image} alt={name} width={600} height={300} />
-                </div>
-                <div className='text-left break-all mr-27 '>
-                    <p className='font-bold text-xl'>{description}</p>
-                    <p className='text-lg'>{instructions}</p>
-                    <p className='text-lg'>{status}</p>
-                    <p className='text-lg'>{type}</p>
-                    <p className='text-lg'>{platforms}</p>
+    return (
+        <div className="wrapper">
+            <Header />
+            {/* min-h-screen */}
+            <div className="hero">
+                <div className="hero-content flex-col ">
+                    <div className="flex fler-row gap-4">
+                        <ReactPlayer url="https://www.youtube.com/watch?v=c0i88t0Kacs" />
+                        <Image src={image} alt={name} width={600} height={300} />
+                    </div>
+                    <div>
+                        <h1 className="text-5xl font-bold">{name}</h1>
+                        <p className="text-2xl">{description}</p>
+                        <p className="py-6">{instructions}</p>
+                        <p className='flex text-lg gap-x-0.5'><span className="font-bold">Type:</span>{type}</p>
+                        <p className='flex text-lg gap-x-0.5'><span className="font-bold">Platforms:</span>{platforms}</p>
+                        <button className="btn btn-primary gap-y-0.5" onClick={() => window.open(url)}>GET</button>
+                    </div>
                 </div>
             </div>
+            {/* <div className="m-6">
+                <div className='flex flex-row ml-12 gap-y-3 gap-x-96'>
+                    <ReactPlayer url="https://www.youtube.com/watch?v=c0i88t0Kacs" />
+
+                </div>
+                <div className="flex flex-col m-4">
+                    <p className="flex flex-auto font-bold text-xl">{name}</p>
+                    <p className='flex flex-auto font-bold text-xl'>{description}</p>
+                    <p className='flex text-lg gap-x-2'>Instruction:{instructions}</p>
+                    <p className='flex text-lg gap-x-2'>Status:{status}</p>
+                    <p className='flex text-lg gap-x-2'>Game:{type}</p>
+                    <p className='flex text-lg gap-x-2'><span className="font-bold">Platforms:</span>{platforms}</p>
+                    <button onClick={() => window.open(url)} className="btn btn-primary m-2">
+                        GET
+                    </button>
+                </div>
+            </div> */}
         </div>
-    </div>
-  )
+
+    )
 }
 
 export default GameDetails
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
     return (
         <div className="flex justify-center items-center">
-            <div className="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin">
+            <div className="w-40 h-40 border-t-4 border-b-4 bg-base-200 rounded-full animate-spin">
             </div>
         </div>
     )
