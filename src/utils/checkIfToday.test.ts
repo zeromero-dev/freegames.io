6c72b03de2652c805edbd7e3a9ee9e8ccd7104ed
Roman <55024478+zeromero-dev@users.noreply.github.com>
2023-02-07 17:55:08 +0200

    Added Vitest, husky, cleaned unused (#21)
    - Vitests crucial tests
    - tests every pre-commit
    - builds every pre-push

--- src/utils/checkIfToday.test.ts
+++ src/utils/checkIfToday.test.ts
@@ -1,9 +1,15 @@
-import {describe, expect, it, test} from '@jest/globals';
-import {checkIfToday} from './checkIfToday';
+import { checkIfToday } from "./checkIfToday";
+import { describe, it, expect, test  } from "vitest";
 
-describe('checkIfToday', () => {
-    test('should return true if the date is today', () => {
+describe("Check if the date is for today", () => {
+    it("should return true if the date is today", () => {
         const date = new Date();
         expect(checkIfToday(date.toISOString())).toBe(true);
+    });
+    
+    it("should return false if the date is not today", () => {
+        const date = new Date();
+        date.setDate(date.getDate() - 1);
+        expect(checkIfToday(date.toISOString())).toBe(false);
     });
 });
